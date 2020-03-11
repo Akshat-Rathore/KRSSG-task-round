a516aa2a90630a501173209494ca8fa885a0b5ef
Akshat-Rathore <54955206+Akshat-Rathore@users.noreply.github.com>
2020-03-11 19:12:39 +0530

    Update sar.py

--- sar.py
+++ sar.py
@@ -2,93 +2,100 @@ import socket
 import sys
 import select
 import time
-import schedule
+import schedule     #TO TIME THE LIFT WORLD
 import threading
-old1=[]
-old2=[]
+old1=[]    #to maintain data of last visited floor lift 1 for repairing purposes!
+old2=[]	   #to maintain data of last visited floor lift 2 for repairing purposes!
+PORT=1038
 class Lift:
 	
-	def __init__(self,floor,state,target):
-		self.floor = floor
-		self.state = state
-		self.target = target
+	def __init__(self,fl,state,trg):
+		self.fl = fl     #present floor of the lift
+		self.state = states    #there are  3 states for the lift:U(Moving Upward),I(Idle)and D(Moving Downward)
+		self.trg = trg    #target floor
 
 class System():
 
 	def __init__(self,Lift1,Lift2):
 
-		Lift1 = Lift(0,'U',0)
-		Lift2 = Lift(0,'D',0)
+		Lift1 = Lift(0,'U',0)   #initially Lift 1 goes upward
+		Lift2 = Lift(0,'D',0)   #initially Lift 2 goes Downward
 
-	def transition(self,Lift1,x):
-		if(Lift1.state=='I'):
-			if(Lift1.floor<Lift1.target):
-				old1.append(Lift1.floor)
-				Lift1.floor+=1
+	def transition(self,Lift1,x):	#transition from 1 state to another
+		if(Lift1.state=='I'):		#if lift is idle
+			if(Lift1.fl<Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl+=1
 				return('U')
-			if(Lift1.floor>Lift1.target):
-				Lift1.floor-=1
+			if(Lift1.fl>Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl-=1
 				return('D')
-			if(Lift1.floor==Lift1.target):
+			if(Lift1.fl==Lift1.trg):
+				old1.append(Lift1.fl)
 				return('I')
-		elif(Lift1.state=='U'):
-			if(Lift1.floor<Lift1.target):
-				Lift1.floor+=1
+		elif(Lift1.state=='U'):		#if lift goes up
+			if(Lift1.fl<Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl+=1
 				return('U')
-			if(Lift1.floor>Lift1.target):
-				Lift1.floor+=1
+			if(Lift1.fl>Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl+=1
 				return('U')
-			if(Lift1.floor==Lift1.target):
+			if(Lift1.fl==Lift1.trg):
 				if(x==1):
-					print("Lift {} reached floor {}".format(x,str(Lift1.floor)))
+					print("Lift {} -> {}".format(x,str(Lift1.fl)))
 				else:
-					print("Lift {} reached floor {}".format(x,str(Lift1.floor))) 
+					print("Lift {} -> {}".format(x,str(Lift1.fl))) 
 				return('I')
-		else:
-			if(Lift1.floor<Lift1.target):
-				Lift1.floor-=1
+		else:				#if lift goes down
+			if(Lift1.fl<Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl-=1
 				return('D')
-			if(Lift1.floor>Lift1.target):
-				Lift1.floor-=1
+			if(Lift1.fl>Lift1.trg):
+				old1.append(Lift1.fl)
+				Lift1.fl-=1
 				return('D')
-			if(Lift1.floor==Lift1.target):
+			if(Lift1.fl==Lift1.trg):
 				if(x==2):
-					print("Lift {}  ->  {}".format(x,str(Lift1.floor)))
+					print("Lift {}  ->  {}".format(x,str(Lift1.fl)))
 				else:
-					print("Lift {}  ->  {}".format(x,str(Lift1.floor))) 
+					print("Lift {}  ->  {}".format(x,str(Lift1.fl))) 
 				return('I')
 
 def main():
 	liftup = Lift(0,'U',0)
 	liftdown = Lift(0,'D',0)
-	syss = System(liftup,liftdown)
+	syss = System(liftup,liftdown)    #defining our lift world
 	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	server.bind((socket.gethostname(),1038))
-	server.listen(2)
+	server.bind((socket.gethostname(),PORT))
+	server.listen(2)        #COZ ONLY 1 CLIENT SOCKET TO HEAR
 	print("Listening")
-	head_size = 10
+	HEADERSIZE = 10
 	lis = []
 	def change(liftup,liftdown):
-		if(((liftup.state!='I')and(liftdown.state!='I'))or(len(lis)==0)):
+		if(((liftup.state!='I')and(liftdown.state!='I'))or(len(lis)==0)):    #NOTHING TO CHANGE
 			return
 		else:
-			if((liftup.state=='I')and(len(listup)!=0)):
-				mi = 100
-				for ele in listup:
-					if(ele<mi):
-						mi = ele
+			if((liftup.state=='I')and(len(listup)!=0)):    #LIFT 1 NEEDS TO MOVE
+				mi = 100			       #HOPE BUILDING DOES'NT HAVE 100 FLOORS ABOVE TE GROUND
+				for i in listup:
+					if(i<mi):
+						mi = i
 				if(mi in listup):
 					listup.remove(mi)
-				liftup.target = mi
+				liftup.trg = mi
 				liftup.state = 'I'
-			if((liftdown.state=='I')and(len(listdown)!=0)):
-				ma = -100
-				for ele in listdown:
-					if(ele>ma):
-						ma = ele
+			if((liftdown.state=='I')and(len(listdown)!=0)):	  # #LIFT 2 NEEDS TO MOVE
+				ma = -100									
+				for i in listdown:
+					if(i>ma):
+						ma = i
 				if(ma in listdown):
 					listdown.remove(ma)
-				liftdown.target = ma
+				liftdown.trg = ma
 				liftdown.state = 'I'
 				return
 	def repeat():
@@ -98,7 +105,7 @@ def main():
 			change(liftup,liftdown)
 			time.sleep(1)
 
-	t1 = threading.Thread(target = lambda:repeat())
+	t1 = threading.Thread(trg = lambda:repeat())
 	t1.start()
 		
 	listup = []
@@ -106,15 +113,16 @@ def main():
 	conn,addr = server.accept()
 	while True:
 
-		msg = conn.recv(head_size)
+		msg = conn.recv(HEADERSIZE)
 		length  = int(msg.decode("utf-8"))
 		full_msg = ""
 		while(len(full_msg)!=length):
 			part = conn.recv(1).decode("utf-8")
 			full_msg += part
+		print("Given Input:")
 		print(full_msg)
 
-		if(full_msg=="exit"):
+		if(full_msg=="out"):					
 			exit()			
 			break
 		else:
@@ -128,5 +136,8 @@ def main():
 				listdown.append(int(li[2]))
 if __name__ =="__main__":
 	main()
+
+
+
 
 
